2c2e866610609c75e7d2313c7c09cad2ccac251d
mirazfaruk <mirazfaruk@gmail.com>
2023-12-01 22:58:47 +0600

    First commit

---
+++ revision.js
@@ -0,0 +1,35 @@
+// Number type variable
+var money = 100;
+var students = 50;
+console.log('Number type variable', money, students);
+
+// String type variable
+var name = 'Miraz';
+var place = 'Basundhara R/A';
+console.log('String type variable', name, place);
+
+// Boolean type variable
+var isYouLoveMe = true;
+var isYouPassTheExam = false;
+console.log('Boolean type variable', isYouLoveMe, isYouPassTheExam);
+
+// Array declaration
+var nameOfFriends = ['miraz', 'topu', 'apon', 'roysa'];
+console.log(nameOfFriends);
+
+var elements = nameOfFriends[2];
+console.log(elements);
+
+nameOfFriends[2] = 'anika';
+console.log(nameOfFriends[2]);
+
+var maleFriend = nameOfFriends.indexOf('topu');
+console.log(maleFriend);
+
+nameOfFriends.push('moitry');
+console.log(nameOfFriends);
+
+nameOfFriends.pop();
+console.log(nameOfFriends);
+
+
